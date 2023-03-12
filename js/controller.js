4609fea9293fb42c388f2500c4eb34d3e34c5178
Tamás Nagy <ntamasalbert@gmail.com>
2023-03-12 15:39:32 +0100

    initialize controller.js and model.js

---
+++ js/controller.js
@@ -0,0 +1,4 @@
+import * as model from "./model.js";
+
+import "core-js/stable";
+import "regenerator-runtime/runtime";
---
+++ js/model.js
@@ -0,0 +1,15 @@
+import { async } from "regenerator-runtime";
+import "regenerator-runtime/runtime";
+
+export const clock = {
+  time: {
+    hour: 0,
+    minute: 0,
+  },
+  zone: "",
+  country: "",
+  date: {
+    month: "",
+    day: 0,
+  },
+};
