3d804e68bac8596ab6ad37bb3535ad118d184842
Tamás Nagy <ntamasalbert@gmail.com>
2023-05-06 14:03:11 +0200

    add final spinning globe

--- js/views/globe.js
+++ js/views/globe.js
@@ -1,26 +1,35 @@
-const world = Globe({ animateIn: false })(
-  document.getElementById('globeViz')
-).globeImageUrl('//unpkg.com/three-globe/example/img/earth-blue-marble.jpg');
-// .bumpImageUrl('//unpkg.com/three-globe/example/img/earth-topology.png');
+const world = Globe({ animateIn: false })(document.querySelector('.globe-box'))
+  .globeImageUrl('../../img/globe/earth.jpg') // Globe image
+  .showAtmosphere(false) // No shadow
+  .backgroundColor('rgba(0,0,0,0)') // No background (default = black)
+  // Dimensions needed to fit container merfectly
+  .height(750)
+  .width(850);
 
 // Auto-rotate
 world.controls().autoRotate = true;
-world.controls().autoRotateSpeed = 0.5;
+world.controls().autoRotateSpeed = 1;
 
-// // Add clouds sphere
-// const CLOUDS_IMG_URL = '../../img/clouds.png'; // from https://github.com/turban/webgl-earth
-// const CLOUDS_ALT = 0.004;
-// const CLOUDS_ROTATION_SPEED = -0.006; // deg/frame
+// Disable zooming in and out and also clicking and roating the globe by the user
+world.controls().enabled = false;
 
-// new THREE.TextureLoader().load(CLOUDS_IMG_URL, cloudsTexture => {
-//   const clouds = new THREE.Mesh(
-//     new THREE.SphereGeometry(world.getGlobeRadius() * (1 + CLOUDS_ALT), 75, 75),
-//     new THREE.MeshPhongMaterial({ map: cloudsTexture, transparent: true })
-//   );
-//   world.scene().add(clouds);
+// Zoon camera in to fit the circle perfectly
+world.camera().zoom = 1.335;
 
-//   (function rotateClouds() {
-//     clouds.rotation.y += (CLOUDS_ROTATION_SPEED * Math.PI) / 180;
-//     requestAnimationFrame(rotateClouds);
-//   })();
-// });
+// Add clouds sphere
+const CLOUDS_IMG_URL = '../../img/globe/clouds.png'; // from https://github.com/turban/webgl-earth
+const CLOUDS_ALT = 0.004;
+const CLOUDS_ROTATION_SPEED = -0.006; // deg/frame
+
+new THREE.TextureLoader().load(CLOUDS_IMG_URL, cloudsTexture => {
+  const clouds = new THREE.Mesh(
+    new THREE.SphereGeometry(world.getGlobeRadius() * (1 + CLOUDS_ALT), 75, 75),
+    new THREE.MeshPhongMaterial({ map: cloudsTexture, transparent: true })
+  );
+  world.scene().add(clouds);
+
+  (function rotateClouds() {
+    clouds.rotation.y += (CLOUDS_ROTATION_SPEED * Math.PI) / 180;
+    requestAnimationFrame(rotateClouds);
+  })();
+});
