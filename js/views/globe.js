26c87ca26c986d9ef3c7992916b9f4f1125ed592
Tamás Nagy <ntamasalbert@gmail.com>
2023-05-16 15:06:45 +0200

    add globe responsibility

--- js/views/globe.js
+++ js/views/globe.js
@@ -1,10 +1,61 @@
 const world = Globe({ animateIn: false })(document.querySelector('.globe-box'))
   .globeImageUrl('../../img/globe/earth.jpg') // Globe image
   .showAtmosphere(false) // No shadow
-  .backgroundColor('rgba(0,0,0,0)') // No background (default = black)
-  // Dimensions needed to fit container merfectly
-  .height(750)
-  .width(850);
+  .backgroundColor('rgba(0,0,0,0)'); // No background (default = black)
+
+// Dimensions needed to fit container merfectly
+world.height(750).width(850);
+
+// Getting current screen width
+let width =
+  window.innerWidth ||
+  document.documentElement.clientWidth ||
+  document.body.clientWidth;
+
+// Making globe responsible for every device (changing height property)
+if (width > 1600) world.height(750);
+if (width <= 1600) world.height(715);
+if (width <= 1400) world.height(750);
+if (width <= 1200) world.height(690);
+if (width <= 950) world.height(750);
+if (width <= 850) world.height(695);
+if (width <= 800) world.height(675);
+if (width <= 770) world.height(655);
+if (width <= 700) world.height(605);
+if (width <= 600) world.height(525);
+if (width <= 550) world.height(460);
+if (width <= 500) world.height(435);
+if (width <= 450) world.height(390);
+if (width <= 420) world.height(370);
+if (width <= 390) world.height(360);
+if (width <= 350) world.height(290);
+
+// Listen for resize event so it works the same when on bigger screens you resize the page it adjusts to the new screen size
+window.addEventListener('resize', () => {
+  // Getting current screen width
+  let width =
+    window.innerWidth ||
+    document.documentElement.clientWidth ||
+    document.body.clientWidth;
+
+  // Making globe responsible for every device (changing height property)
+  if (width > 1600) world.height(750);
+  if (width <= 1600) world.height(715);
+  if (width <= 1400) world.height(750);
+  if (width <= 1200) world.height(690);
+  if (width <= 950) world.height(750);
+  if (width <= 850) world.height(695);
+  if (width <= 800) world.height(675);
+  if (width <= 770) world.height(655);
+  if (width <= 700) world.height(605);
+  if (width <= 600) world.height(525);
+  if (width <= 550) world.height(460);
+  if (width <= 500) world.height(435);
+  if (width <= 450) world.height(390);
+  if (width <= 420) world.height(370);
+  if (width <= 390) world.height(360);
+  if (width <= 350) world.height(290);
+});
 
 // Auto-rotate
 world.controls().autoRotate = true;
